8133074e0cf9f4aa33952e150d872c27d3f8dcbd
erbe1 <erik.bergstrom86@gmail.com>
2019-01-09 10:27:59 +0100

    Ignore file Och projekt

---
+++ OrchestreAcademy/OrchestreAcademy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrchestreAcademy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
